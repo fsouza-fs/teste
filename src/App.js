123ac621e8677dad44704c6ff3a2cd74566135cd
felipes <felipes@naehas.com>
2022-12-29 21:42:40 -0300

    Muda frase inicial

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Essa é uma aplicação de exemplo</h1>
+      <h1>A pipeline funcionou!!</h1>
     </div>
   );
 }
